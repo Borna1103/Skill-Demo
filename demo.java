a5c7b328a1d52df3166fbebd4b43d16c354be428
Borna1103 <Borna1103@msn.com>
2022-05-01 21:09:32 -0700

    changes

---
+++ demo.java
@@ -0,0 +1,5 @@
+public class demo{
+    public static int adder(int a, int b){
+        return a+b;
+    }
+}
---
+++ demotest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class demotest {
+    @Test
+    public void addition() {
+        assertEquals(2, demo.adder(2,1));
+    }
+}
